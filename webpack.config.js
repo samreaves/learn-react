57207c9d1adc2e037888ff7431f188be94996637
Sam <thesamreaves@gmail.com>
2015-11-11 19:36:47 -0500

    Initial commit from first class at Egghead.io. Slightly irked I have to commit ./dist since the html file is not handled yet by webpack. Perhaps that's later?

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	entry: "./src/js/index.js",
+	output: {
+		path: "./dist",
+		filename: "bundle.js",
+		publicPath: "/"
+	},
+	devServer: {
+		inline: true,
+		contentBase: "./dist"
+	},
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel',
+			query: {
+				presets: ['es2015', 'react']
+			}
+		}]
+	}
+};
